7fc435a86e8d9203cd2502f73530fdadab881e9e
Naveed Jooma <naveed@viam.com>
2025-02-03 14:32:10 -0500

    Initial commit

---
+++ models/module.go
@@ -0,0 +1,130 @@
+package models
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"go.viam.com/rdk/components/generic"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/resource"
+)
+
+var (
+	Keypresser = resource.NewModel("njooma", "keystrokes", "keypresser")
+)
+
+func init() {
+	resource.RegisterComponent(generic.API, Keypresser,
+		resource.Registration[resource.Resource, *Config]{
+			Constructor: newKeystrokesKeypresser,
+		},
+	)
+}
+
+type Config struct {
+	DelayMS uint8 `json:"delay_ms"`
+	resource.TriviallyValidateConfig
+}
+
+type keystrokesKeypresser struct {
+	name resource.Name
+
+	logger logging.Logger
+	cfg    *Config
+
+	cancelCtx  context.Context
+	cancelFunc func()
+
+	resource.TriviallyReconfigurable
+	resource.TriviallyCloseable
+}
+
+func newKeystrokesKeypresser(ctx context.Context, deps resource.Dependencies, rawConf resource.Config, logger logging.Logger) (resource.Resource, error) {
+	conf, err := resource.NativeConfig[*Config](rawConf)
+	if err != nil {
+		return nil, err
+	}
+
+	cancelCtx, cancelFunc := context.WithCancel(context.Background())
+
+	s := &keystrokesKeypresser{
+		name:       rawConf.ResourceName(),
+		logger:     logger,
+		cfg:        conf,
+		cancelCtx:  cancelCtx,
+		cancelFunc: cancelFunc,
+	}
+	return s, nil
+}
+
+func (s *keystrokesKeypresser) Name() resource.Name {
+	return s.name
+}
+
+func (s *keystrokesKeypresser) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
+	type KeypressType string
+	const (
+		Sequential   KeypressType = "sequential"
+		Simultaneous KeypressType = "simultaneous"
+	)
+	type Keystroke struct {
+		Type KeypressType `json:"type"`
+		Keys []string     `json:'keys"`
+	}
+	type Command struct {
+		Keystrokes []Keystroke `json:"keystrokes"`
+	}
+
+	jsonbody, err := json.Marshal(cmd)
+	if err != nil {
+		return nil, fmt.Errorf("could not convert command into JSON: %w", err)
+	}
+
+	command := Command{}
+	if err := json.Unmarshal(jsonbody, &command); err != nil {
+		return nil, fmt.Errorf("could not unmarshall command JSON to appropriate type: %w", err)
+	}
+
+	for _, keystroke := range command.Keystrokes {
+		if keystroke.Type == Simultaneous {
+			pressed := []int{}
+			for _, keys := range keystroke.Keys {
+				// Check if meta key and press/release immediately
+				// Otherwise, go rune by rune
+				if key, ok := keymap[keys]; ok {
+					Press(key)
+					pressed = append(pressed, key)
+				} else {
+					for _, r := range keys {
+						if key := GetKey(r); key >= 0 {
+							Press(key)
+							pressed = append(pressed, key)
+						}
+					}
+				}
+			}
+			for i := len(pressed) - 1; i >= 0; i-- {
+				Release(pressed[i])
+			}
+		} else if keystroke.Type == Sequential {
+			for _, keys := range keystroke.Keys {
+				// Check if meta key and press/release immediately
+				// Otherwise, go rune by rune
+				if key, ok := keymap[keys]; ok {
+					Press(key)
+					Release(key)
+				} else {
+					for _, r := range keys {
+						if key := GetKey(r); key >= 0 {
+							Press(key)
+							Release(key)
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nil, nil
+}
---
+++ models/sendkeys.go
@@ -0,0 +1,87 @@
+package models
+
+import (
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+var user32 = syscall.NewLazyDLL("user32.dll")
+var sendInputProc = user32.NewProc("SendInput")
+
+type keyboardInput struct {
+	wVk         uint16
+	wScan       uint16
+	dwFlags     uint32
+	time        uint32
+	dwExtraInfo uint64
+}
+
+type input struct {
+	inputType uint32
+	ki        keyboardInput
+	padding   uint64
+}
+
+const flag_KeyboardInput = 1
+const flag_KeyUp = 0x0002
+
+func keyWithShift(key int) (int, bool) {
+	if key < 0xFFF {
+		return key, false
+	}
+	return key - 0xFFF, true
+}
+
+func keyDown(key int) error {
+	input := input{inputType: flag_KeyboardInput, ki: keyboardInput{wVk: uint16(key)}}
+	if ret, _, err := sendInputProc.Call(
+		uintptr(1),
+		uintptr(unsafe.Pointer(&input)),
+		uintptr(unsafe.Sizeof(input)),
+	); ret == 0 {
+		return err
+	}
+	return nil
+}
+
+func keyUp(key int) error {
+	input := input{inputType: flag_KeyboardInput, ki: keyboardInput{wVk: uint16(key), dwFlags: flag_KeyUp}}
+	if ret, _, err := sendInputProc.Call(
+		uintptr(1),
+		uintptr(unsafe.Pointer(&input)),
+		uintptr(unsafe.Sizeof(input)),
+	); ret == 0 {
+		return err
+	}
+	return nil
+}
+
+func Press(key int) error {
+	key, needsShift := keyWithShift(key)
+	if needsShift {
+		if err := keyDown(keymap["VK_SHIFT"]); err != nil {
+			return err
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	if err := keyDown(key); err != nil {
+		return err
+	}
+	time.Sleep(100 * time.Millisecond)
+	return nil
+}
+
+func Release(key int) error {
+	key, needsShift := keyWithShift(key)
+	if needsShift {
+		defer func() {
+			keyUp(keymap["VK_SHIFT"])
+		}()
+	}
+	if err := keyUp(key); err != nil {
+		return err
+	}
+	time.Sleep(100 * time.Millisecond)
+	return nil
+}
