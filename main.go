78864a1144a9c6c575ad0392af21782353a90ae3
Max Horowitz <maxhorowitz@gmail.com>
2025-02-07 00:10:36 -0500

    subproc working

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"keystrokes/models"
+	"os"
+
+	"github.com/viam-labs/screenshot-cam/subproc"
+	"go.viam.com/rdk/components/generic"
 	"go.viam.com/rdk/module"
 	"go.viam.com/rdk/resource"
-	"go.viam.com/rdk/components/generic"
 )
 
 func main() {
-	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
-	module.ModularMain(resource.APIModel{ generic.API, models.Keypresser})
+	logger := module.NewLoggerFromArgs("screenshot-cam")
+	var arg string
+	if len(os.Args) >= 2 {
+		arg = os.Args[1]
+	}
+	switch arg {
+	case "parent":
+		// parent is a test mode for spawning a child proc directly from session 0 CLI. see README.md for instructions.
+		if err := subproc.SpawnSelf(" child"); err != nil {
+			panic(err)
+		}
+	case "child":
+		// child is the subprocess started in session 1 by a session 0 parent. it does the work.
+		logger.Info("doing a keystrokes test instead of starting module")
+	default:
+		// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
+		module.ModularMain(resource.APIModel{generic.API, models.Keypresser})
+	}
 }
