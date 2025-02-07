d09cccfd3b6b8bc2c6dc4839f44f96d5e7a2ebaa
Abe Winter <abe.winter@viam.com>
2025-02-07 14:45:53 -0500

    very messy but works

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
 	"keystrokes/models"
 	"os"
 	"time"
@@ -27,22 +29,28 @@ func SendKeys() {
 func main() {
 	logger := module.NewLoggerFromArgs("screenshot-cam")
 	var arg string
-	if len(os.Args) >= 2 {
+	var arg2 string
+	if len(os.Args) >= 3 {
 		arg = os.Args[1]
+		arg2 = os.Args[2]
 	}
 	switch arg {
 	case "parent":
+		arg2 := base64.StdEncoding.EncodeToString([]byte(`{"keystrokes": [{"type":"sequential", "keys": ["A", "B", "C"]}]}`))
 		// parent is a test mode for spawning a child proc directly from session 0 CLI. see README.md for instructions.
-		if err := subproc.SpawnSelf(" child"); err != nil {
+		if err := subproc.SpawnSelf(" child " + arg2); err != nil {
 			panic(err)
 		}
 	case "child":
+		jsonArg, err := base64.StdEncoding.DecodeString(arg2)
+		if err != nil {
+			panic(err)
+		}
 		// child is the subprocess started in session 1 by a session 0 parent. it does the work.
 		logger.Info("child mode: doing a keystrokes test instead of starting module")
-		// if err := models.DemoMode(context.Background(), logger); err != nil {
-		// 	panic(err)
-		// }
-		SendKeys()
+		if err := models.DemoMode(context.Background(), logger, jsonArg); err != nil {
+			panic(err)
+		}
 	default:
 		// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
 		module.ModularMain(resource.APIModel{generic.API, models.Keypresser})
--- models/module.go
+++ models/module.go
@@ -2,9 +2,12 @@ package models
 
 import (
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 
+	"github.com/micmonay/keybd_event"
+	"github.com/viam-labs/screenshot-cam/subproc"
 	"go.viam.com/rdk/components/generic"
 	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/resource"
@@ -62,109 +65,121 @@ func (s *keystrokesKeypresser) Name() resource.Name {
 	return s.name
 }
 
-func (s *keystrokesKeypresser) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
-	type KeypressType string
-	const (
-		Sequential   KeypressType = "sequential"
-		Simultaneous KeypressType = "simultaneous"
-	)
-	type Keystroke struct {
-		Type KeypressType `json:"type"`
-		Keys []string     `json:"keys"`
-	}
-	type Command struct {
-		Keystrokes []Keystroke `json:"keystrokes"`
-	}
+type KeypressType string
 
+const (
+	Sequential   KeypressType = "sequential"
+	Simultaneous KeypressType = "simultaneous"
+)
+
+type Keystroke struct {
+	Type KeypressType `json:"type"`
+	Keys []string     `json:"keys"`
+}
+type Command struct {
+	Keystrokes []Keystroke `json:"keystrokes"`
+}
+
+func (s *keystrokesKeypresser) DoCommand(ctx context.Context, cmd map[string]interface{}) (map[string]interface{}, error) {
 	jsonbody, err := json.Marshal(cmd)
 	if err != nil {
 		return nil, fmt.Errorf("could not convert command into JSON: %w", err)
 	}
 
-	command := Command{}
-	if err := json.Unmarshal(jsonbody, &command); err != nil {
-		return nil, fmt.Errorf("could not unmarshall command JSON to appropriate type: %w", err)
+	// todo: only do subproc when needed; it won't work in non-service mode
+
+	jsonArg := base64.StdEncoding.EncodeToString(jsonbody)
+	// parent is a test mode for spawning a child proc directly from session 0 CLI. see README.md for instructions.
+	if err := subproc.SpawnSelf(" child " + jsonArg); err != nil {
+		panic(err)
 	}
 
-	for _, keystroke := range command.Keystrokes {
-		if keystroke.Type == Simultaneous {
-			pressed := []int{}
-			for _, keys := range keystroke.Keys {
-				// Check if meta key and press/release immediately
-				// Otherwise, go rune by rune
-				if key, ok := keymap[keys]; ok {
-					if err := Press(key); err != nil {
-						return nil, err
-					}
-					pressed = append(pressed, key)
-				} else {
-					for _, r := range keys {
-						if key := GetKey(r); key >= 0 {
-							if err := Press(key); err != nil {
-								return nil, err
-							}
-							pressed = append(pressed, key)
-						}
-					}
-				}
-			}
-			for i := len(pressed) - 1; i >= 0; i-- {
-				if err := Release(pressed[i]); err != nil {
-					return nil, err
-				}
-			}
-		} else if keystroke.Type == Sequential {
-			for _, keys := range keystroke.Keys {
-				// Check if meta key and press/release immediately
-				// Otherwise, go rune by rune
-				if key, ok := keymap[keys]; ok {
-					if err := Press(key); err != nil {
-						return nil, err
-					}
-					if err := Release(key); err != nil {
-						return nil, err
-					}
-				} else {
-					for _, r := range keys {
-						if key := GetKey(r); key >= 0 {
-							if err := Press(key); err != nil {
-								return nil, err
-							}
-							if err := Release(key); err != nil {
-								return nil, err
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+	// for _, keystroke := range command.Keystrokes {
+	// 	if keystroke.Type == Simultaneous {
+	// 		pressed := []int{}
+	// 		for _, keys := range keystroke.Keys {
+	// 			// Check if meta key and press/release immediately
+	// 			// Otherwise, go rune by rune
+	// 			if key, ok := keymap[keys]; ok {
+	// 				if err := Press(key); err != nil {
+	// 					return nil, err
+	// 				}
+	// 				pressed = append(pressed, key)
+	// 			} else {
+	// 				for _, r := range keys {
+	// 					if key := GetKey(r); key >= 0 {
+	// 						if err := Press(key); err != nil {
+	// 							return nil, err
+	// 						}
+	// 						pressed = append(pressed, key)
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 		for i := len(pressed) - 1; i >= 0; i-- {
+	// 			if err := Release(pressed[i]); err != nil {
+	// 				return nil, err
+	// 			}
+	// 		}
+	// 	} else if keystroke.Type == Sequential {
+	// 		for _, keys := range keystroke.Keys {
+	// 			// Check if meta key and press/release immediately
+	// 			// Otherwise, go rune by rune
+	// 			if key, ok := keymap[keys]; ok {
+	// 				if err := Press(key); err != nil {
+	// 					return nil, err
+	// 				}
+	// 				if err := Release(key); err != nil {
+	// 					return nil, err
+	// 				}
+	// 			} else {
+	// 				for _, r := range keys {
+	// 					if key := GetKey(r); key >= 0 {
+	// 						if err := Press(key); err != nil {
+	// 							return nil, err
+	// 						}
+	// 						if err := Release(key); err != nil {
+	// 							return nil, err
+	// 						}
+	// 					}
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// }
 
 	return nil, nil
 }
 
-func DemoMode(ctx context.Context, logger logging.Logger) error {
+func DemoMode(ctx context.Context, logger logging.Logger, jsonArg []byte) error {
 	// cancelCtx, cancelFunc := context.WithCancel(context.Background())
 
-	kp := &keystrokesKeypresser{
-		logger: logger,
-		cfg:    &Config{},
-		// cancelCtx:  cancelCtx,
-		// cancelFunc: cancelFunc,
-	}
-	demo := `{
-		"keystrokes": [
-			{"type": "simultaneous", "keys": ["VK_META"]},
-			{"type": "sequential", "keys": ["notepad", "VK_ENTER"]},
-			{"type": "sequential", "keys": ["Hello", " ", "World"]},
-			{"type": "simultaneous", "keys": ["VK_SHIFT", "1"]}
-		]
-	}`
-	var cmd map[string]any
-	err := json.Unmarshal([]byte(demo), &cmd)
+	// demo := `{
+	// 	"keystrokes": [
+	// 		{"type": "simultaneous", "keys": ["VK_META"]},
+	// 		{"type": "sequential", "keys": ["notepad", "VK_ENTER"]},
+	// 		{"type": "sequential", "keys": ["Hello", " ", "World"]},
+	// 		{"type": "simultaneous", "keys": ["VK_SHIFT", "1"]}
+	// 	]
+	// }`
+	var command Command
+	err := json.Unmarshal(jsonArg, &command)
 	if err != nil {
 		return err
 	}
-	_, err = kp.DoCommand(ctx, cmd)
-	return err
+	for _, keystroke := range command.Keystrokes {
+		kb, err := keybd_event.NewKeyBonding()
+		if err != nil {
+			return err
+		}
+		for range keystroke.Keys {
+			println("SETTING KEY A")
+			kb.SetKeys(keybd_event.VK_A)
+		}
+		if err := kb.Launching(); err != nil {
+			return err
+		}
+		println("OK LAUNCH")
+	}
+	return nil
 }
